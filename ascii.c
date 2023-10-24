a358a9c903d780ff50036f7825f5d669c172d26a
MrLinusMeow <>
2023-10-24 14:24:08 +0200

    Creating git repository

--- ascii.c
+++ ascii.c
@@ -1,114 +1,116 @@
 #include <stdio.h>
 
 void main(void) {
-	for(int i = 0x00; i != 0x80; ++i) {
+	printf("ASCII TABLE\n");
+	for(signed short i = 0x00; i != 0x80; ++i) {
 		switch(i){
 			default:
-				printf("%c\t", i);
+				printf("\e[97m@%c\e[0m\t", i);
 				break;
 			case 0:
-				printf("NUL\t");
+				printf("\e[96mNUL\t");
 				break;
 			case 1:
-				printf("SOH\t");
+				printf("\e[96mSOH\t");
 				break;
 			case 2:
-				printf("STX\t");
+				printf("\e[96mSTX\t");
 				break;
 			case 3:
-				printf("ETX\t");
+				printf("\e[96mETX\t");
 				break;
 			case 4:
-				printf("EOT\t");
+				printf("\e[96mEOT\t");
 				break;
 			case 5:
-				printf("ENQ\t");
+				printf("\e[96mENQ\t");
 				break;
 			case 6:
-				printf("ACK\t");
+				printf("\e[96mACK\t");
 				break;
 			case 7:
-				printf("BEL\t");
+				printf("\e[96mBEL\t");
 				break;
 			case 8:
-				printf("BS\t");
+				printf("\e[96mBS\t");
 				break;
 			case 9:
-				printf("HT\t");
+				printf("\e[96mHT\t");
 				break;
 			case 10:
-				printf("LF\t");
+				printf("\e[96mLF\t");
 				break;
 			case 11:
-				printf("VT\t");
+				printf("\e[96mVT\t");
 				break;
 			case 12:
-				printf("FF\t");
+				printf("\e[96mFF\t");
 				break;
 			case 13:
-				printf("CR\t");
+				printf("\e[96mCR\t");
 				break;
 			case 14:
-				printf("SO\t");
+				printf("\e[96mSO\t");
 				break;
 			case 15:
-				printf("SI\t");
+				printf("\e[96mSI\t");
 				break;
 			case 16:
-				printf("DLE\t");
+				printf("\e[96mDLE\t");
 				break;
 			case 17:
-				printf("DC1\t");
+				printf("\e[96mDC1\t");
 				break;
 			case 18:
-				printf("DC2\t");
+				printf("\e[96mDC2\t");
 				break;
 			case 19:
-				printf("DC3\t");
+				printf("\e[96mDC3\t");
 				break;
 			case 20:
-				printf("DC4\t");
+				printf("\e[96mDC4\t");
 				break;
 			case 21:
-				printf("NAK\t");
+				printf("\e[96mNAK\t");
 				break;
 			case 22:
-				printf("SYN\t");
+				printf("\e[96mSYN\t");
 				break;
 			case 23:
-				printf("ETB\t");
+				printf("\e[96mETB\t");
 				break;
 			case 24:
-				printf("CAN\t");
+				printf("\e[96mCAN\t");
 				break;
 			case 25:
-				printf("EM\t");
+				printf("\e[96mEM\t");
 				break;
 			case 26:
-				printf("SUB\t");
+				printf("\e[96mSUB\t");
 				break;
 			case 27:
-				printf("ESC\t");
+				printf("\e[96mESC\t");
 				break;
 			case 28:
-				printf("FS\t");
+				printf("\e[96mFS\t");
 				break;
 			case 29:
-				printf("GS\t");
+				printf("\e[96mGS\t");
 				break;
 			case 30:
-				printf("RS\t");
+				printf("\e[96mRS\t");
 				break;
 			case 31:
-				printf("US\t");
+				printf("\e[96mUS\t");
 				break;
 			case 127:
-				printf("DEL\t");
-				break;
+				printf("\e[96mDEL\e[0m\t");
+				break; 
 		}
-		printf("0x%x\t", i);
-		printf("%d\t", i);
-		printf("0%o\t", i);
-		printf("%bb\n", i);
+		printf("\e[95mx%x\e[0m\t", i);
+		printf("\e[32md%d\e[0m\t", i);
+		printf("\e[93mo%o\e[0m\t", i);
+		printf("\e[91mb%b\e[0m\n", i);
 	}
+	printf("\e[0m");
 }
